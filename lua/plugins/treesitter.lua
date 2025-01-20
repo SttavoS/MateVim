92f4398039ba3996f1cf477e33e6921ebd3872d0
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-01-19 22:02:27 -0300

    add kdl (zellij) to treesitter highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,41 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				ensure_installed = {
-					"lua",
-					"javascript",
-					"typescript",
-					"tsx",
-					"html",
-					"css",
-					"scss",
-					"vue",
-					"rust",
-					"elixir",
-					"heex",
-					"eex",
-					"python",
-					"c_sharp",
-					"fsharp",
-					"php",
-					"sql",
-					"json",
-					"yaml",
-					"xml",
-					"dockerfile",
-					"markdown",
-					"markdown_inline",
-					"norg",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+      configs.setup({
+        ensure_installed = {
+          "lua",
+          "javascript",
+          "typescript",
+          "tsx",
+          "html",
+          "css",
+          "scss",
+          "vue",
+          "rust",
+          "elixir",
+          "heex",
+          "eex",
+          "python",
+          "c_sharp",
+          "fsharp",
+          "php",
+          "sql",
+          "json",
+          "yaml",
+          "xml",
+          "dockerfile",
+          "markdown",
+          "markdown_inline",
+          "norg",
+          "kdl"
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
