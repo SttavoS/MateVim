1b4f312e33ab5090f5f6f1b87bee58dedfa3be98
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-01-31 10:35:14 -0300

    WIP: change to picker from telescope and neo-tree
    Snacks.picker is not ready yet, wait 2 weeks

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -34,6 +34,7 @@ return {
       bufdelete = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
+      picker = { enabled = true },
       quickfile = { enabled = true },
       rename = { enabled = true },
       statuscolumn = { enabled = true },
@@ -70,6 +71,33 @@ return {
         end,
         desc = "Lazygit Log (cwd)",
       },
+      -- Picker
+      { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
+      { "<leader>/",       function() Snacks.picker.grep() end,            desc = "Grep" },
+      { "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader><space>", function() Snacks.picker.files() end,           desc = "Find Files" },
+      -- File Explorer
+      {
+        "<leader>e",
+        function()
+          Snacks.picker.explorer()
+        end,
+        desc = "Open File Explorer"
+      },
+      -- find
+      { "<leader>fb", function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
+      { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      { "<leader>ff", function() Snacks.picker.files() end,                                   desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
+      { "<leader>fr", function() Snacks.picker.recent() end,                                  desc = "Recent" },
+      -- git
+      { "<leader>gc", function() Snacks.picker.git_log() end,                                 desc = "Git Log" },
+      { "<leader>gs", function() Snacks.picker.git_status() end,                              desc = "Git Status" },
+      -- Grep
+      { "<leader>sb", function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
+      { "<leader>sB", function() Snacks.picker.grep_buffers() end,                            desc = "Grep Open Buffers" },
+      { "<leader>sg", function() Snacks.picker.grep() end,                                    desc = "Grep" },
+      { "<leader>sw", function() Snacks.picker.grep_word() end,                               desc = "Visual selection or word", mode = { "n", "x" } },
       -- Refactor
       {
         "<leader>cR",
@@ -111,11 +139,11 @@ return {
         diagnostics = "nvim_lsp",
         offsets = {
           {
-            filetype = "neo-tree",
+            filetype = "picker",
             text = "File Explorer",
             highlight = "Directory",
             text_align = "left",
-            separator = true,
+            -- separator = true,
           },
         },
       },
