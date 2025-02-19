e9e5302ea1c672d2781311a18caa2e9e1b62d815
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-02-19 19:06:13 -0300

    configure neotree to always show somes dotfiles

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -19,4 +19,26 @@ return {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim",            -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          ".git",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          ".gitignored",
+        },
+        always_show_by_pattern = { -- uses glob style patterns
+          ".env*",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          ".DS_Store",
+          "thumbs.db",
+        },
+      },
+    },
+  },
 }
