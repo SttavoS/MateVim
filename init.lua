b168f41448cabd6b1f6c88152d849c502f6d25be
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-02-24 20:35:19 -0300

    revert config for coloscheme

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,17 +2,21 @@ return {
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
-    config = true,
-		opts = {
-			overrides = {
-				["@tag"] = { fg = "#8ec07c", bg = "NONE", bold = false, italic = false },
-				["@tag.builtin"] = { fg = "#8ec07c", bg = "NONE", bold = false, italic = false },
-				["@tag.attribute"] = { fg = "#fabd2f", bg = "NONE", bold = false, italic = false },
-				["@tag.delimiter"] = { fg = "#83a598", bg = "NONE", bold = false, italic = false },
-				["@string.javascript"] = { fg = "#b8bb26", bg = "NONE", bold = false, italic = false },
-			},
-			dim_inactive = false,
-			transparent_mode = false,
-		},
+		config = function()
+			require("gruvbox").setup({
+				overrides = {
+					["@tag"] = { fg = "#8ec07c", bg = "NONE", bold = false, italic = false },
+					["@tag.builtin"] = { fg = "#8ec07c", bg = "NONE", bold = false, italic = false },
+					["@tag.attribute"] = { fg = "#fabd2f", bg = "NONE", bold = false, italic = false },
+					["@tag.delimiter"] = { fg = "#83a598", bg = "NONE", bold = false, italic = false },
+					["@string.javascript"] = { fg = "#b8bb26", bg = "NONE", bold = false, italic = false },
+				},
+				dim_inactive = false,
+				transparent_mode = false,
+			})
+
+			vim.o.background = "dark" -- or "light" for light mode
+			vim.cmd([[colorscheme gruvbox]])
+		end,
 	},
 }
