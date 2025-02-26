915edc8603c342394a94d9d43055cfa880b93ff4
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-02-25 21:46:33 -0300

    change the colors for some highlight groups

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,8 +11,13 @@ return {
 					["@tag.attribute"] = { fg = "#fabd2f", bg = "NONE", bold = false, italic = false },
 					["@tag.delimiter"] = { fg = "#83a598", bg = "NONE", bold = false, italic = false },
 					["@string.javascript"] = { fg = "#b8bb26", bg = "NONE", bold = false, italic = false },
+          -- snacks.dashboard
+          SnacksDashboardDesc = { fg = '#ebdbb2' },
 					-- snacks.picker
+					SnacksPickerDirectory = { fg = "#ebdbb2" },
 					SnacksPickerDir = { fg = "#928374" },
+					SnacksPickerPathHidden = { fg = "#928374" },
+					SnacksPickerGitStatusUntracked = { fg = "#98971a" },
 				},
 				dim_inactive = false,
 				transparent_mode = false,
