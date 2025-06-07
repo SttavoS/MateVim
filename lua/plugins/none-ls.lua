6184f881e24facaf62ca5ff5adf8f00457f70aef
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-06-07 13:15:31 -0300

    disable none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,41 +1,42 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	{
+		"nvimtools/none-ls.nvim",
+		enabled = false,
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          -- null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.phpcsfixer,
-          -- null_ls.builtins.diagnostics.phpcs,
-        },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({
-              group = augroup,
-              buffer = bufnr,
-            })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-    keys = {
-      {
-        "<leader>gf",
-        vim.lsp.buf.format,
-        desc = "Code format",
-      },
-    },
-  },
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					-- null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.phpcsfixer,
+					-- null_ls.builtins.diagnostics.phpcs,
+				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({
+							group = augroup,
+							buffer = bufnr,
+						})
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = bufnr })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+		keys = {
+			{
+				"<leader>cf",
+				vim.lsp.buf.format,
+				desc = "Code format",
+			},
+		},
+	},
 }
