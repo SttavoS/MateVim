3bd5a511175193038ec0e05643ed0dda2b8933cd
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2026-05-13 23:48:40 -0300

    refact formatting.lua

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,38 +1,45 @@
 return {
-	{
-		"stevearc/conform.nvim",
-    event = {"BufReadPre", "BufNewFile"},
-		enabled = true,
-		dependencies = { "mason.nvim" },
-		lazy = true,
-		cmd = "ConformInfo",
-		keys = {
-			{
-				"<leader>cf",
-				function(args)
-					require("conform").format({ bufnr = args.bufnr })
-				end,
-			},
-			{
-				"<leader>cF",
-				function()
-					require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-				end,
-				mode = { "n", "v" },
-				desc = "Format Injected Langs",
-			},
-		},
-		opts = {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    cmd = "ConformInfo",
+    keys = {
+      {
+        "<leader>cf",
+        function()
+          require("conform").format({ async = true })
+        end,
+        mode = { "n", "v" },
+        desc = "Format buffer",
+      },
+      {
+        "<leader>cF",
+        function()
+          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+        end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
+    opts_extend = { "formatters_by_ft" },
+    opts = {
       default_format_opts = {
         timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
-        lsp_format = "fallback", -- not recommended to change
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
       },
-			formatters_by_ft = {
-				lua = { "stylua" },
-				php = { "phpcs" },
-				javascript = { "prettier" },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 3000, lsp_format = "fallback" }
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        php = { "php_cs_fixer" },
+        javascript = { "prettier" },
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
         typescriptreact = { "prettier" },
@@ -41,8 +48,22 @@ return {
         html = { "prettier" },
         json = { "prettier" },
         yaml = { "prettier" },
-        markdown = { "prettier"}
-			},
-		},
-	},
+        markdown = { "prettier" },
+      },
+    },
+    init = function()
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, { desc = "Disable autoformat-on-save", bang = true })
+
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, { desc = "Re-enable autoformat-on-save" })
+    end,
+  },
 }
