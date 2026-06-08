c7335f6dbe6e9ce33df746770e4744cc0f94fe53
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2026-06-08 17:00:14 -0300

    add Quality of Life plugins
    render-markdown.nvim
    nvim-colorizer.lua
    yanky.nvim
    diffview.nvim

---
+++ lua/plugins/qol.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    opts = {
+      file_types = { "markdown" },
+      code = { sign = false, width = "block", right_pad = 1 },
+      heading = { sign = false },
+    },
+  },
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      filetypes = { "*", "!lazy" },
+      user_default_options = {
+        names = false,
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+        tailwind = true,
+        sass = { enable = true, parsers = { "css" } },
+        mode = "background",
+      },
+    },
+  },
+  {
+    "gbprod/yanky.nvim",
+    opts = {
+      ring = { storage = "shada" },
+    },
+    keys = {
+      { "<leader>p", "<cmd>YankyRingHistory<cr>", desc = "Yank History" },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
+      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
+      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
+      { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle Forward Through Yank History" },
+      { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Through Yank History" },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History (current)" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "File History (branch)" },
+    },
+    opts = {},
+  },
+}
