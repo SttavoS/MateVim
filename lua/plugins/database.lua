8ee3a36d1c54b762485ce2274ae4d88550bf1840
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2026-06-11 14:43:01 -0300

    fix vim-dadbod-ui plugin

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,11 +1,11 @@
 return {
   {
-    "tpope/vim-dadbod",
-    cmd = { "DB", "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    "kristijanhusak/vim-dadbod-ui",
     dependencies = {
-      { "kristijanhusak/vim-dadbod-ui", cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" } },
-      "kristijanhusak/vim-dadbod-completion",
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_show_database_icon = 1
