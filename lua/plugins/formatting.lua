a8ae46376195433bc1260297aeb024c9ddb1fd8a
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2026-06-12 12:41:09 -0300

    fix format for PHP

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -22,7 +22,6 @@ return {
         desc = "Format Injected Langs",
       },
     },
-    opts_extend = { "formatters_by_ft" },
     opts = {
       default_format_opts = {
         timeout_ms = 3000,
@@ -31,11 +30,18 @@ return {
         lsp_format = "fallback",
       },
       format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        local ft = vim.bo[bufnr].filetype
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat or vim.g["disable_autoformat_" .. ft] then
           return
         end
         return { timeout_ms = 3000, lsp_format = "fallback" }
       end,
+      formatters = {
+        php_cs_fixer = {
+          args = { "fix", "--rules=@PSR12", "--using-cache=no", "$FILENAME" },
+          exit_codes = { 0, 1 },
+        },
+      },
       formatters_by_ft = {
         lua = { "stylua" },
         php = { "php_cs_fixer" },
@@ -55,15 +61,21 @@ return {
       vim.api.nvim_create_user_command("FormatDisable", function(args)
         if args.bang then
           vim.b.disable_autoformat = true
+        elseif args.args ~= "" then
+          vim.g["disable_autoformat_" .. args.args] = true
         else
           vim.g.disable_autoformat = true
         end
-      end, { desc = "Disable autoformat-on-save", bang = true })
+      end, { desc = "Disable autoformat-on-save (optional: filetype)", bang = true, nargs = "?" })
 
-      vim.api.nvim_create_user_command("FormatEnable", function()
-        vim.b.disable_autoformat = false
-        vim.g.disable_autoformat = false
-      end, { desc = "Re-enable autoformat-on-save" })
+      vim.api.nvim_create_user_command("FormatEnable", function(args)
+        if args.args ~= "" then
+          vim.g["disable_autoformat_" .. args.args] = false
+        else
+          vim.b.disable_autoformat = false
+          vim.g.disable_autoformat = false
+        end
+      end, { desc = "Re-enable autoformat-on-save (optional: filetype)", nargs = "?" })
     end,
   },
 }
