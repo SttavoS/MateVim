dade7dcc49b0802fe7a64c5f1a4ea7209c0f83ec
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2026-06-18 00:05:46 -0300

    add sql format patterns

--- lua/plugins/lang/sql.lua
+++ lua/plugins/lang/sql.lua
@@ -19,8 +19,32 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        sql_formatter = {
+          prepend_args = function()
+            local config = vim.fn.json_encode({
+              language = "postgresql",
+              keywordCase = "upper",
+              dataTypeCase = "upper",
+              functionCase = "upper",
+              identifierCase = "preserve",
+              indentStyle = "standard",
+              logicalOperatorNewline = "before",
+              expressionWidth = 80,
+              linesBetweenQueries = 2,
+              denseOperators = false,
+              newlineBeforeSemicolon = false,
+              tabWidth = 2,
+              useTabs = false,
+            })
+            return { "--config", config }
+          end,
+        },
+      },
       formatters_by_ft = {
         sql = { "sql_formatter" },
+        mysql = { "sql_formatter" },
+        plsql = { "sql_formatter" },
       },
     },
   },
