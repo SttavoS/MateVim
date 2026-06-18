d5dea1df4be8f26860ab5bc702dfcd07326a6526
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2026-06-18 00:05:12 -0300

    open dadbod-ui panel in right side

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -9,7 +9,7 @@ return {
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_show_database_icon = 1
-      vim.g.db_ui_win_position = "left"
+      vim.g.db_ui_win_position = "right"
       vim.g.db_ui_use_nvim_notify = 1
       vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui"
     end,
